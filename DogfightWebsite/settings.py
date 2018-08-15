4842c80f6906e09e2d3ba66d46ff6cb2f64ce7c9
Lucas Kruk <lucaskruk13@gmail.com>
2018-08-15 05:06:06 -0500

    added MYSQL Support

--- DogfightWebsite/settings.py
+++ DogfightWebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import secret
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'DogfightWebsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': secret.NAME,
+        'USER': secret.USER,
+        'PASSWORD': secret.PASSWORD,
+        'HOST': secret.HOST,
     }
 }
 
