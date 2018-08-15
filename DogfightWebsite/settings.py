363705f6f0ca785d574aa6f18b420a065edc4cf6
Lucas Kruk <lucaskruk13@gmail.com>
2018-08-15 06:04:55 -0500

    Implemented Signup Backend to save user & created form template

--- DogfightWebsite/settings.py
+++ DogfightWebsite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'feed',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
 # Create your views here.
 def signup(request):
-    return render(request, 'accounts/auth/signup.html')
+
+    if request.method=='POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('feed')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'accounts/auth/signup.html', {'form':form})
