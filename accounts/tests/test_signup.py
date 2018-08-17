2ded69f7222a1402cf1c413e4c75d2fc237d465d
Lucas Kruk <lucaskruk13@gmail.com>
2018-08-17 05:17:00 -0500

    Added Invalid form Tests

--- accounts/tests/test_signup.py
+++ accounts/tests/test_signup.py
@@ -4,6 +4,7 @@ from accounts.views import signup
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from accounts.models import Profile
+from accounts.forms import ProfileForm
 
 # Create your tests here.
 class SignupTests(TestCase):
@@ -84,7 +85,7 @@ class SuccessfulSignupUpTests(TestCase):
         user = self.feed_response.context.get('user')
 
         self.assertEquals('Its Me!', user.profile.bio)
-        self.assertEquals(5.0, user.profile.handicap)
+        self.assertEquals(5.0, float(user.profile.handicap))
 
 class ProfileCreation(TestCase):
     def setUp(self):
@@ -100,7 +101,9 @@ class ProfileCreation(TestCase):
 
         }
 
+
         self.response = self.client.post(url, data)
+
 
     def test_profile_initial_value(self):
         # A profile should exist
@@ -109,7 +112,7 @@ class ProfileCreation(TestCase):
         # The Profile should have a Initial Value, with a zero handicap
         profile = Profile.objects.first()
         self.assertTrue(profile.initial)
-        self.assertEquals(profile.handicap, 0)
+        self.assertEquals(float(profile.handicap), 0)
 
     def test_profile_update(self):
         account_url = reverse('my_account')
@@ -123,7 +126,7 @@ class ProfileCreation(TestCase):
 
         profile = Profile.objects.first() # Should be updated
 
-        self.assertEquals(1.4, profile.handicap) # ensure profile handicap is updated
+        self.assertEquals(1.4, float(profile.handicap)) # ensure profile handicap is updated
         self.assertRedirects(proile_response, reverse('feed')) # Should redirect to Feed
 
 # TODO: test invalid haidicap setup, example - Superuser doesnt initially have a handicap
@@ -147,3 +150,47 @@ class InvalidSignUpTests(TestCase):
 
     def test_dont_create_user(self):
         self.assertFalse(User.objects.exists())
+
+class InvalidProfileSignup(TestCase):
+
+    def setUp(self):
+        url = reverse('signup')
+
+        data = {
+            'username': 'john',
+            'email': 'john@appleseed.com',
+            'first_name': 'john',
+            'last_name': 'appleseed',
+            'password1': 'abcdef123456',
+            'password2': 'abcdef123456',
+
+        }
+
+        self.response = self.client.post(url, data)
+
+
+        data = {
+            'handicap': '2..5',
+            'bio': 'its me',
+        }
+
+        # post an invalid form
+        self.account_post = self.client.post(reverse('my_account'), data)
+        self.account_response = self.client.get(reverse('my_account'))
+
+
+    def test_account_status_code(self):
+
+        self.assertEquals(self.account_post.status_code, 200)
+
+
+    def test_csrf(self):
+        self.assertContains(self.account_response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.account_response.context.get('form')
+        self.assertIsInstance(form, ProfileForm)
+
+    def test_form_errors_invalid_handicap(self):
+        form = self.account_post.context.get('form')
+        self.assertTrue(form.errors)
